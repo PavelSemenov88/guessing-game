7654939fbc7a287afa937eaa6eb3f0fe3148984e
Pavel <cemenru@mail.ru>
2023-02-21 23:38:53 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNumb = min;
+        this.maxNumb = max;
     }
 
     guess() {
-
+        console.log('guess', this.number = Math.round((this.maxNumb + this.minNumb) / 2));
+        return this.number = Math.round((this.maxNumb + this.minNumb) / 2);
     }
 
     lower() {
-
+        console.log('lower', this.maxNumb = this.number);
+        return this.maxNumb = this.number;
     }
 
     greater() {
-
+        console.log('greater', this.minNumb = this.number);
+        return this.minNumb = this.number;
     }
 }
 
